503c1bdbbb2cf17ed20246edbc0b79401ed2148b
Kai Sells <kai.sells@hotmail.co.uk>
2020-08-05 12:12:12 +0100

    Updated ergast API links to https

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -9,26 +9,26 @@ export class DataService {
   constructor(private http: HttpClient) {}
 
   getNextRace(): Observable<any> {
-    return this.http.get('http://ergast.com/api/f1/current/next.json');
+    return this.http.get('https://ergast.com/api/f1/current/next.json');
   }
 
   getLastRace(): Observable<any> {
-    return this.http.get('http://ergast.com/api/f1/current/last/results.json');
+    return this.http.get('https://ergast.com/api/f1/current/last/results.json');
   }
 
   getDriverStandings(): Observable<any> {
     return this.http.get(
-      'http://ergast.com/api/f1/current/driverStandings.json'
+      'https://ergast.com/api/f1/current/driverStandings.json'
     );
   }
 
   getConstructorStandings(): Observable<any> {
     return this.http.get(
-      'http://ergast.com/api/f1/current/constructorStandings.json'
+      'https://ergast.com/api/f1/current/constructorStandings.json'
     );
   }
 
   getCalendar(): Observable<any> {
-    return this.http.get('http://ergast.com/api/f1/current.json');
+    return this.http.get('https://ergast.com/api/f1/current.json');
   }
 }
